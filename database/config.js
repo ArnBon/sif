c2008b055f1301699260ee69de9b44579588f395
ArnBon <bonillaarnaldo@gmail.com>
2025-04-15 09:25:34 -0400

    2 Se afinaron detalles de configuracion de bd errores corregidos

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.connect(process.env.DB_CONN));
+        await mongoose.connect(process.env.DB_CONN);
         console.log('BD On-line...')
         
     } catch (error) {
--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
+// 1.- crear el servidor de express
 const app = express();
 
+// 2. cors es un middleware que se ejecuta desde aqui hacia abajo
 app.use( cors() );
 
+// 3.- ejecuta BD
+dbConnection();
+
+// creando las rutas del servicio
 app.get('/', (req, res) => {
     res.json({
         ok: true,
         msg: 'Hola mundo!'
     });
 
-});  
+}); 
 
+// 4. Se crean las Rutas
+
+
+
+// 5.- ejecutar el servidor
 app.listen(3000, () => {
 console.log('Servidor on-line' + 3000);
 });
