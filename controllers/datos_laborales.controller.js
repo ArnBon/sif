b3859667d8b9ecb398ad2b95c72341356782706f
ArnBon <bonillaarnaldo@gmail.com>
2025-07-21 20:06:59 -0400

    Datos laborales terminado faltan las validaciones

--- controllers/datos_laborales.controller.js
+++ controllers/datos_laborales.controller.js
@@ -1,14 +1,24 @@
 const { response } = require('express');
 const DatosLaborales = require('../models/datos_laborales.model');
-
-
+const Persona = require('../models/personas.model');
 
 
 const getdatosLaborales = async (req, res = response) => {
-    res.json({
-        ok:true,
-        msg: 'Obtener datos laborales'
-    });
+    try {
+        // Obtener todos los datos laborales
+        const datosLaborales = await DatosLaborales.find({}, 'id_datos_laboral id_persona profesion ocupacion actividad_economica ingreso_anual empresa_trabaja fecha_ingreso');
+        res.json({
+            ok: true,
+            datosLaborales // Devuelve la lista de datos laborales
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al obtener los datos laborales.'
+        });        
+    }   
 }
 
 
@@ -23,29 +33,92 @@ const getdatosLaboralesId = async (req, res = response) => {
 
 
 const creardatosLaborales = async (req, res = response) => {
+
+    const datosLaborales = new DatosLaborales(req.body); // Crear el objeto DatosLaborales
+    try {
+    await datosLaborales.save(); // Guardar en la base de datos
     res.json({
-        ok:true,
-        msg: 'Crear datos laborales'
-    });
+        ok: true,
+        msg: 'Datos laborales creados correctamente',
+        datosLaborales // Devuelve el objeto creado
+    });    
+} catch (error) {
+    console.error(error);
+    res.status(500).json({
+        ok: false,
+        msg: 'Error al crear los datos laborales.'
+    });    
+   }    
 }
 
 
 
 const actualizardatosLaborales = async (req, res = response) => {
-    res.json({
-        ok:true,
-        msg: 'Actualizar datos laborales'
-    });
+    const dlid = req.params.id;
+
+    try {
+        //encuentra los datos laborales por ID
+        const datosLaboralesDB = await DatosLaborales.findById(dlid)
+        if (!datosLaboralesDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existen datos laborales por ese ID.'
+            });
+        }
+        //2.- Actualiza el registro por ese dlid
+        const campos = req.body;
+
+        //2.2.- elimianr campos que no se pueden actualizar
+        //delete campos.profesion //este es un ejemplo realmente no lo necesito
+
+        //3.- Actuzlizar el dato laboral en la bd
+        const datosLaboralesActualizados = await DatosLaborales.findByIdAndUpdate(dlid, campos, { new: true });
+        res.json({
+            ok: true,
+            msg: 'Datos laborales actualizados correctamente',
+            datosLaborales: datosLaboralesActualizados // Devuelve el objeto actualizado
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al actualizar los datos laborales.'
+        });        
+    }    
 }
 
 
 
 const eliminardatosLaborales = async (req, res = response) => {
-    res.json({
+     const dlid = req.params.id;
+     try {
+        //1.- Encuentra los datos laborales por ID
+        //encuentra los datos laborales por ID
+        const datosLaboralesDB = await DatosLaborales.findById(dlid)
+        
+        if (!datosLaboralesDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existen datos laborales por ese ID.'
+            });
+        }
+        //2.- Elimina el registro por ese dlid
+        await DatosLaborales.findByIdAndDelete(dlid);
+
+        res.json({
         ok:true,
         msg: 'Eliminar datos laborales'
     });
-}
+        
+     } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                ok: false,
+                msg: 'Error al eliminar los datos laborales.'
+            });
+        }
+     }
+
 
 module.exports = {
     getdatosLaborales,
