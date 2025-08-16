f7149a8f4924a331b298b2051bda0aa513997173
ArnBon <bonillaarnaldo@gmail.com>
2025-08-16 10:42:11 -0400

    controlador de plan completado

--- controllers/planes.controller.js
+++ controllers/planes.controller.js
@@ -1,32 +1,96 @@
 const { response } = require('express');
 const Plan = require('../models/planes.model');
 
-const getPlanes = (req, res = response) => {
+
+const getPlanes = async (req, res = response) => {
+
+try {
+  //obtener todos los planes
+  const planes = await  Plan.find({},'id_plan nombre_plan suma_asegurada');  
   res.json({
     ok: true,
-    msg: 'getPlanes',
+    planes // Devuelve la lista de planes,
   });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({
+    ok: false,
+    msg: 'Error al obtener los planes',
+  });
+}
 };
 
-const crearPlan = (req, res = response) => {
-  res.json({
+const crearPlan = async (req, res = response) => {
+
+  const plan = new Plan(req.body) //crear el objeto Plan
+try {
+  await plan.save(); //guardar en la base de datos
+
+  res.status(201).json({
     ok: true,
-    msg: 'crearPlan',
+    plan,
   });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({
+    ok: false,
+    msg: 'Error al crear el plan',
+  });
+}
 };
 
-const actualizarPlan = (req, res = response) => {
+
+const actualizarPlan = async (req, res = response) => {
+  const pid = req.params.id;
+
+try {  
+  const planDB = await Plan.findById(pid);
+
+    if (!planDB) {
+      return res.status(404).json({
+      ok: true,
+      msg: 'No existe plan',
+    });    
+  }
+  //actualiza elplan en la bd
+  const edicionPlan = await Plan.findByIdAndUpdate(pid, req.body, {new: true});
   res.json({
     ok: true,
-    msg: 'actualizarPlan',
+    plan: edicionPlan,
+  });  
+} catch (error) {
+  console.error(error);
+  res.status(500).json({
+    ok: false,
+    msg: 'Error al actualizar el plan',
   });
+}
 };
 
-const eliminarPlan = (req, res = response) => {
+const eliminarPlan = async (req, res = response) => {
+  const pid = req.params.id;
+
+try {
+  const planDB = await Plan.findById(pid);
+
+  if (!planDB) {
+      return res.status(404).json({
+      ok: true,
+      msg: 'No existe plan',
+    });    
+  }
+  await Plan.findByIdAndDelete(pid);
   res.json({
     ok: true,
-    msg: 'eliminarPlan',
+    msg: 'Plan eliminado',
   });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({
+    ok: false,
+    msg: 'Error al eliminar el plan',
+  });
+}
 };
 
 module.exports = {
