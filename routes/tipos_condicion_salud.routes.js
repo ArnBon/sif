d697b75d415b21dcdad9732f0d210f158e3687b3
ArnBon <bonillaarnaldo@gmail.com>
2025-10-05 10:23:43 -0400

    se completaron las rutas para crud completo en tipo condicion salud y tipo intervenciones

--- routes/tipos_condicion_salud.routes.js
+++ routes/tipos_condicion_salud.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-const {getTiposCondicionSalud} = require();
+const {getTiposCondicionSalud, getTipoCondicionSaludId, crearTipoCondicionSalud, actualizarTipoCondicionSalud,eliminarTipoCondicionSalud } = require('../controllers/tipos_condicion_salud.controller');
 
 const router = Router();
 
 router.get('/', getTiposCondicionSalud);
+router.get('/:id', getTipoCondicionSaludId);
+router.post('/', crearTipoCondicionSalud);
+router.put('/:id', actualizarTipoCondicionSalud);
+router.delete('/:id', eliminarTipoCondicionSalud);
 
 
 module.exports = router;
--- routes/tipos_intervencion.routes.js
+++ routes/tipos_intervencion.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
-const {getTiposIntervencion} = require();
+const {getTiposIntervencion,getTiposIntervencionId, crearTiposIntervencion, actualizarTiposIntervencion, eliminarTiposIntervencion} = require('../controllers/tipos_intervencion.controller');
 
 const router = Router();
 
 router.get('/', getTiposIntervencion);
+router.get('/:id', getTiposIntervencionId);
+router.post('/', crearTiposIntervencion);
+router.put('/:id', actualizarTiposIntervencion);
+router.delete('/:id', eliminarTiposIntervencion);
 
 module.exports = router;
